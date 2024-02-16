50f8d2bba31d06c2f7fb6277186b7c3bcaecc090
milar111 <114105495+milar111@users.noreply.github.com>
2024-02-16 21:34:03 +0200

    task6

---
+++ pointers/task5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void swap_elements(void *a, void *b) {
+    int temp = *(int*)a;
+    *(int*)a = *(int*)b;
+    *(int*)b = temp;
+}
+
+void bubblesort(void *array, int count, int (*compare)(const void *, const void *), void (*swap_elements)(void *, void *)) {
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = 0; j < count - i - 1; j++) {
+            if (compare(array + j, array + j + 1) > 0) {
+                swap_elements(array + j, array + j + 1);
+            }
+        }
+    }
+}
+
+int ascending_compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int descending_compare(const void *a, const void *b) {
+    return (*(int*)b - *(int*)a);
+}
+
+
+int main() {
+    int arr[] = {5, 2, 8, 1, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Sorted array in ascending order: ");
+    bubblesort(arr, n, ascending_compare, swap_elements);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    printf("Sorted array in descending order: ");
+    bubblesort(arr, n, descending_compare, swap_elements);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
