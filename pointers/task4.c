eded8ab6ce4ba2a12cfc15029712c981091e5d40
milar111 <114105495+milar111@users.noreply.github.com>
2024-02-16 18:58:22 +0200

    task4

--- pointers/task4.c
+++ pointers/task4.c
@@ -12,6 +12,20 @@ int compareacs(const void * a, const void * b )
     }
 }
 
+int comparedesk(const void * a, const void * b)
+{
+    int p1 = *(const int *)a;
+    int p2 = *(const int *)b;
+
+    if (p1 > p2)
+        return -1;
+    else if (p1 < p2)
+        return 1;
+    else
+        return 0;
+}
+
+
 #include <stdio.h>
 int main(void)
 {
@@ -20,6 +34,8 @@ int a = 15;
 int b = 10;
 
 printf("%d", compareacs(&a,&b));
+printf("\n");
+printf("%d", comparedesk(&a, &b));
 
 return 0;
 }
