35b9adcb4997e386360c3971482f2b2e77c2b51c
milar111 <114105495+milar111@users.noreply.github.com>
2024-02-29 08:35:10 +0200

    task2

--- PointersAndArrays/task2.c
+++ PointersAndArrays/task2.c
@@ -9,8 +9,8 @@ void swap(int *a, int *b) {
 void sortArray(int arr[], int size) {
     int i, j;
 
-    for (i = 0; i < size - 1; i++) {
-        for (j = 0; j < size - i - 1; j++) {
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
             if (*(arr + j) > *(arr + j + 1)) {
                 swap(arr + j, arr + j + 1);
             }
