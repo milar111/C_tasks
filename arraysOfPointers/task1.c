c2b3d5b523f4e1d2ccce4793a8d0ae9a311bba56
milar111 <114105495+milar111@users.noreply.github.com>
2024-03-21 10:09:29 +0200

    using if/else instead of switch

--- arraysOfPointers/task1.c
+++ arraysOfPointers/task1.c
@@ -7,13 +7,17 @@
 
 void remove_newline(char *str) {
     char *newline = strchr(str, '\n');
-    if (newline != NULL) *newline = '\0';
+    if (newline != NULL) {
+    *newline = '\0';
+}
 }
 
 int main() {
     int num_subjects, num_students = 0;
     char **subjects = malloc(MAX_SUBJECTS * sizeof(char*));
-    for (int i = 0; i < MAX_SUBJECTS; i++) subjects[i] = malloc((MAX_NAME_LENGTH + 1) * sizeof(char));
+    for (int i = 0; i < MAX_SUBJECTS; i++) {
+        subjects[i] = malloc((MAX_NAME_LENGTH + 1) * sizeof(char));
+    }
 
     printf("Enter the number of subjects: ");
     scanf("%d", &num_subjects);
@@ -21,7 +25,7 @@ int main() {
 
     if (num_subjects <= 0) {
         printf("Error: Number of subjects must be a positive integer.\n");
-        return 1; 
+        return 1;
     }
 
     printf("Enter the names of the subjects:\n");
@@ -38,57 +42,57 @@ int main() {
     do {
         printf("\nMenu:\n1. Enter a student\n2. Print diary\n3. Exit\nEnter your choice: ");
         scanf("%d", &choice);
-        getchar(); 
+        getchar();
 
-        switch (choice) {
-            case 1: {
-                students = realloc(students, (num_students + 1) * sizeof(char*));
-                students[num_students] = malloc((MAX_NAME_LENGTH + 1) * sizeof(char));
-                printf("Enter student's name: ");
-                fgets(students[num_students], MAX_NAME_LENGTH + 1, stdin);
-                remove_newline(students[num_students]);
+        if (choice == 1) {
+            students = realloc(students, (num_students + 1) * sizeof(char*));
+            students[num_students] = malloc((MAX_NAME_LENGTH + 1) * sizeof(char));
+            printf("Enter student's name: ");
+            fgets(students[num_students], MAX_NAME_LENGTH + 1, stdin);
+            remove_newline(students[num_students]);
 
-                grades = realloc(grades, (num_students + 1) * sizeof(int*));
-                grades[num_students] = malloc(num_subjects * sizeof(int));
+            grades = realloc(grades, (num_students + 1) * sizeof(int*));
+            grades[num_students] = malloc(num_subjects * sizeof(int));
 
-                printf("Enter grades for each subject:\n");
-                int sum_grades = 0; 
-                for (int i = 0; i < num_subjects; i++) {
-                    scanf("%d", &grades[num_students][i]);
-                    sum_grades += grades[num_students][i]; 
-                }
-                total_grades = realloc(total_grades, (num_students + 1) * sizeof(int));
-                total_grades[num_students] = sum_grades;
-
-                num_students++;
-                break;
+            printf("Enter grades for each subject:\n");
+            int sum_grades = 0;
+            for (int i = 0; i < num_subjects; i++) {
+                scanf("%d", &grades[num_students][i]);
+                sum_grades += grades[num_students][i];
             }
-            case 2: {
-                printf("%-15s", "");
-                for (int i = 0; i < num_subjects; i++) printf("%-15s", subjects[i]);
-                printf("%-15s\n", "AVG");
+            total_grades = realloc(total_grades, (num_students + 1) * sizeof(int));
+            total_grades[num_students] = sum_grades;
 
-                for (int i = 0; i < num_students; i++) {
-                    printf("%-15s", students[i]);
-                    for (int j = 0; j < num_subjects; j++) printf("%-15d", grades[i][j]);
-                    float avg_grade = (float)total_grades[i] / num_subjects;
-                    printf("%-15.2f\n", avg_grade);
-                }
-                break;
+            num_students++;
+        } else if (choice == 2) {
+            printf("%-15s", "");
+            for (int i = 0; i < num_subjects; i++) {
+                printf("%-15s", subjects[i]);
             }
-            case 3:
-                for (int i = 0; i < MAX_SUBJECTS; i++) free(subjects[i]);
-                free(subjects);
-                for (int i = 0; i < num_students; i++) { 
-                    free(students[i]); 
-                    free(grades[i]); 
+            printf("%-15s\n", "AVG");
+
+            for (int i = 0; i < num_students; i++) {
+                printf("%-15s", students[i]);
+                for (int j = 0; j < num_subjects; j++) {
+                    printf("%-15d", grades[i][j]);
                 }
-                free(students);
-                free(grades); 
-                free(total_grades);
-                break;
-            default:
-                printf("Invalid choice. Please enter a number between 1 and 3.\n");
+                float avg_grade = (float)total_grades[i] / num_subjects;
+                printf("%-15.2f\n", avg_grade);
+            }
+        } else if (choice == 3) {
+            for (int i = 0; i < MAX_SUBJECTS; i++) {
+                free(subjects[i]);
+            }
+            free(subjects);
+            for (int i = 0; i < num_students; i++) {
+                free(students[i]);
+                free(grades[i]);
+            }
+            free(students);
+            free(grades);
+            free(total_grades);
+        } else {
+            printf("Invalid choice. Please enter a number between 1 and 3.\n");
         }
     } while (choice != 3);
 
