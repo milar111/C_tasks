8ab3bbb2106ed53f12609fb294b34a0701400d1a
milar111 <114105495+milar111@users.noreply.github.com>
2024-04-10 23:04:43 +0300

    task3
    WorkingVersion

--- Complex data types - exercise/task3.c
+++ Complex data types - exercise/task3.c
@@ -1,12 +1,10 @@
-// Направете програма, която да предоставя на потребителя меню с три опции - добавяне на ученик, извеждане на списък със всички ученици и изход. При въвеждането на ученик трябва да се въведе ученик с име, което може да е до 15 символа, неговият номер в клас и средния му успех (променлива от тип double). За извеждането на ученици трябва да се принтира информация за всеки ученик от класа - неговото име, номер в клас и среден успех. За целта използвайте структура, която да пази информацията на масива и заделяне на динамична памет за масива от ученици.
-
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_LENGTH 15
 
 void printLegend()
 {
-    printf("1 - Add student");
+    printf("\n1 - Add student");
     printf("\n2 - Print Diary");
     printf("\n3 - exit");
 }
@@ -17,23 +15,22 @@ typedef struct student{
     double avgGrade;
 }Student;
 
-//Don't know why it works ->
-void addStudent(Student *students, int count)
+
+int addStudent(Student *students, int count)
 {
-    printf("\nEnter the Student's Name: ");
+    printf("Enter the Student's Name: ");
     scanf("%s", &students[count].name);
-    printf("\nEnter the Student's Number: ");
+    printf("Enter the Student's Number: ");
     scanf("%d" ,&students[count].studentNum);
-    printf("\nEnter the Student's Grade: ");
+    printf("Enter the Student's Grade: ");
     scanf("%lf" ,&students[count].avgGrade);
-    count++;
 }
 
 void printDiary(const Student *students, const int count){
     if(count == 0){
-        printf("No students");
+        printf("\nNo students");
     }else{
-    printf("Diary:");
+    printf("\nDiary:");
     for(int i  = 0 ; i < count ; i++){
         printf("\nName : %s \nNumber:%d\nGrade:%lf\n",students[i].name,students[i].studentNum,students[i].avgGrade);
     }
@@ -55,14 +52,16 @@ int main(void){
             case 1:
                 student = realloc(student,(count+1)*sizeof(Student));
                 if (student == NULL){
-                    printf("Handle the zero pouinter");
+                    printf("\nHandle the zero pouinter");
                     }
-                addStudent(student, count + 1);
+                addStudent(student, count);
+                count++;
                 break;
             case 2:
                 printDiary(student, count);
                 break;
             case 3:
+                free(student);
                 exit(0);
         }
     }
