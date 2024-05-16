5461f63061bdb321bce5e48c26e0e98b359332b0
milar111 <114105495+milar111@users.noreply.github.com>
2024-05-16 13:04:22 +0300

    Create exer.c

---
+++ exer.c
@@ -0,0 +1,13 @@
+// typedef struct student{
+//     char  name[20 + 1];
+//     int studentNum;
+//     double avgGrade;
+// }Student;
+
+
+// int addStudent(Student *students, int count){
+//     printf("Student name");
+//     scanf("%s", &students[count].name);
+//     printf("student number");
+
+// }
